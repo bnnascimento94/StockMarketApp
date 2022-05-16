bcb6099758013c2dbd250f6ddce55dff6ec318be
brunon-at-224486729257 <brunon-at-224486729257>
2022-05-16 17:11:25 -0300

    first commit

---
+++ app/src/main/java/com/plcoding/stockmarketapp/di/AppModule.kt
@@ -0,0 +1,39 @@
+package com.plcoding.stockmarketapp.di
+
+import android.app.Application
+import androidx.room.Room
+import com.plcoding.stockmarketapp.data.local.StockDatabase
+import com.plcoding.stockmarketapp.data.remote.StockApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideStockApi(): StockApi{
+        return Retrofit.Builder()
+            .baseUrl(StockApi.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create(StockApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideStickDatabase(app:Application): StockDatabase{
+        return Room.databaseBuilder(
+            app,
+            StockDatabase::class.java,
+            "stock.db"
+        ).build()
+    }
+}
---
+++ app/src/main/java/com/plcoding/stockmarketapp/presentation/company_listings/CompanyListingEvent.kt
@@ -0,0 +1,7 @@
+package com.plcoding.stockmarketapp.presentation.company_listings
+
+sealed class CompanyListingEvent{
+    object Refresh: CompanyListingEvent()
+    data class OnSearchQueryChange(val query: String): CompanyListingEvent()
+
+}
